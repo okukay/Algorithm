2d799ae98e84b84fbf672928fadd9274ee02ce33
needmoney <80242565+NEEDMONEY2@users.noreply.github.com>
2023-07-31 14:07:37 +0900

    Create 2525.py
    오븐 시계

---
+++ 조건문/2525.py
@@ -0,0 +1,13 @@
+hour,min = map(int,input().split())
+timer = int(input())
+
+hour = hour + timer // 60   # timer 몫 더해줌
+min = min + timer % 60      # timer 나머지 더해줌
+
+if min >= 60:               # 그래도 min 이 60이 넘을경우 
+    hour = hour +1
+    min = min -60
+
+if hour >= 24:
+    hour = hour -24
+print(hour,min)
