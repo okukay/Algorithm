094c60e7909288538076372bc56731b74570f5af
needmoney <80242565+NEEDMONEY2@users.noreply.github.com>
2023-07-31 14:15:12 +0900

    Update 2525.py

--- 조건문/2525.py
+++ 조건문/2525.py
@@ -11,3 +11,17 @@ if min >= 60:               # 그래도 min 이 60이 넘을경우
 if hour >= 24:
     hour = hour -24
 print(hour,min)
+
+// 또는 
+
+hour,min = map(int,input().split())
+timer = int(input())
+
+min = min + timer
+
+if min >=60:
+    hour = hour + (min // 60)
+    min =  (min % 60)
+if hour >= 24 :
+    hour = hour -24
+print(hour,min)
