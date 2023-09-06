f8acac9f8cbf9ede3d569c3a071b7e438a45c8d9
needmoney <80242565+NEEDMONEY2@users.noreply.github.com>
2023-09-07 00:12:04 +0900

    Create 24263.py
    알고리즘 수업 - 알고리즘의 수행 시간 2
    뒤로 갈수록 명령어의 정확한 실행 횟수를 구하기 까다로워질 것입니다. 그러나...

---
+++ 시간 복잡도/24263.py
@@ -0,0 +1,15 @@
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         sum <- sum + A[i]; # 코드1
+#     return sum;
+# }
+
+# for 문이 실행될때 시간복잡도 
+# 주어진 함수의 for문이 항상 n에 비례하므로,
+# 수행 횟수는 n, 수행 시간은 항상 1이 된다. (상수 시간이 소요되면 0 , n에 비례하면 1 , 2n에 비례하면 2)
+
+n = input()
+
+print(n)
+print(1)
