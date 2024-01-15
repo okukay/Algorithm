55705dc5a647e774d995c6acde4a1cfd62322e1d
soulkey <80242565+soulkey4@users.noreply.github.com>
2024-01-16 01:23:29 +0900

    Update 9205.py

--- Gold/9205.py
+++ Gold/9205.py
@@ -1,35 +1,38 @@
-def bfs(sj,si,ej,ei):
-    # q생성, 필요변수 등 생성
-    q = []
-    v = [0]*N
+# 맥주 마시면서 걸어가기 문제
 
-    # 큐에 초기데이터 삽입, si,sj는 v에 표시X
-    q.append((sj,si))
+# 편의점만 v[] 배열을 만들어주고 방문하면서 마지막 편의점부터 , 최종목적지 까지의 거리가 1000이하라면 happy 하게 풀이
+
+
+
+def bfs(sj, si, ej, ei):
+    q = []
+    v = [0] * N
+
+    q.append((sj, si))
 
     while q:
-        cj,ci = q.pop(0)
-        if abs(cj-ej)+abs(ci-ei)<=1000:         # 목적지 도달가능
-            return 'happy'
-
-        # 미방문 모든 편의점좌표: 범위내인지 체크
+        cj, ci = q.pop(0)
+        if abs(cj - ej) + abs(ci - ei) <= 1000:          # 편의점에서 목적지 까지 거리가 1000이하 라면 happy
+            return "happy"
+            
         for i in range(N):
-            if v[i]==0:     # 미방문 편의점
-                nj,ni = lst[i]
-                if abs(cj-nj)+abs(ci-ni)<=1000: # 범위내
-                    q.append((nj,ni))
-                    v[i]=1
+            if v[i] == 0:                                # 편의점방문
+                nj, ni = lst[i]                          # 다음 좌표가 = 편의점좌표[i]
+                if abs(cj - nj) + abs(ci - ni) <= 1000:
+                    q.append((nj, ni))
+                    v[i] = 1
+    return "sad"
 
-    return 'sad'
 
 TC = int(input())
 for _ in range(TC):
-    N = int(input())
-    sj, si = map(int, input().split())
-    lst = []
+    N = int(input())                            # 편의점 개수
+    sj, si = map(int, input().split())          # 출발위치
+    lst = []                                    # 편의점 위치 넣을 곳
     for _ in range(N):
-        tj, ti = map(int, input().split())
-        lst.append((tj,ti))
-    ej, ei = map(int, input().split())
+        tj, ti = map(int, input().split())      # 편의점 위치
+        lst.append((tj, ti))
+    ej, ei = map(int, input().split())          # 도착위치
 
-    ans = bfs(sj,si,ej,ei)
-    print(ans)
+    result = bfs(sj, si, ej, ei)
+    print(result)
