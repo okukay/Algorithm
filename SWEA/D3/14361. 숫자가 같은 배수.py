eeb420a3178c0fff6cdec3dbaddb0f3436b6f348
DUK <80242565+soulkey4@users.noreply.github.com>
2024-04-16 23:43:03 +0900

    Create 14361. 숫자가 같은 배수.py

---
+++ SWEA/D3/14361. 숫자가 같은 배수.py
@@ -0,0 +1,15 @@
+num = input()
+
+#원본
+num_list = list(num)
+
+new = num
+# i만큼 곱한 값
+new = list(str(int(num) * i))
+
+# 둘다 sort해서 비교
+sort_num = sorted(num_list)
+sort_new = sorted(new)
+
+while:
+    i = 2
