ff02066d73c08e9c2dfc162906d4ba640407f8d0
김동욱 <80242565+okukay@users.noreply.github.com>
2025-10-24 16:09:44 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.06 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,20 +1,28 @@
-//그리디 문제, 
+// 그리디 문제, 
 // 아이디어 : 작은수 x 큰수 짝지으면 최솟값나옴
+
 import java.util.*;
 class Solution
 {
     public int solution(int []A, int []B){
         Arrays.sort(A);
-        Integer[] arrB = new Integer[B.length];
-
-        for(int i = 0; i<B.length; i++){
-            arrB[i] = B[i];
+        Arrays.sort(B);
+        
+        for(int i = 0; i<B.length / 2; i++){
+            int temp = B[i];
+            B[i] = B[B.length -1 -i];
+            B[B.length-1-i] = temp;
         }
-        Arrays.sort(arrB, Collections.reverseOrder());
+//         Integer[] arrB = new Integer[B.length];
+
+//         for(int i = 0; i<B.length; i++){
+//             arrB[i] = B[i];
+//         }
+//         Arrays.sort(arrB, Collections.reverseOrder());
 
         int answer = 0;
         for(int i = 0; i<A.length; i++){
-            answer += A[i] * arrB[i];
+            answer += A[i] * B[i];
         }
         return answer;
     }
